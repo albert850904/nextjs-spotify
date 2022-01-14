fe89a5e060bdaf33b302689761a8397bb1fce1cd
kairu.cheng <kairu.cheng@ubitusinc.com>
2022-01-14 11:08:26 +0800

    complete styling and functionality

--- components/Player.js
+++ components/Player.js
@@ -1,6 +1,21 @@
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
+import {
+  SwitchHorizontalIcon,
+  HeartIcon,
+  VolumeUpIcon as VolumeDownIcon,
+} from '@heroicons/react/outline';
+import {
+  RewindIcon,
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  VolumeUpIcon,
+} from '@heroicons/react/solid';
+import debounce from 'lodash';
+
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSpotify from '../hooks/useSpotify';
 import useSongInfo from '../hooks/useSongInfo';
@@ -14,7 +29,7 @@ function Player() {
   const [currentTrackId, setCurrentTrackId] =
     useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-  const [volume, setVolume] = useState(50);
+  const [volume, setVolume] = useState(DEFAULT_VOLUME);
 
   const fetchCurrentSong = () => {
     if (!songInfo) {
@@ -24,11 +39,28 @@ function Player() {
       });
 
       spotifyApi.getMyCurrentPlaybackState().then((data) => {
-        console.log(data);
         setIsPlaying(data.body?.is_playing);
       });
     }
   };
+
+  const playPauseHandler = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
+  const debouncedAdjustVolume = useCallback(() => {
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((error) => {});
+    }, 500);
+  }, []);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
@@ -37,14 +69,64 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session]);
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
+
   return (
-    <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       {/* left - song info */}
-      <div>
-        <img
-          className="h-hidden md:inline h-10 w-10"
-          src={songInfo?.album.images?.[0].url}
-          alt={songInfo?.name}
+      <div className="flex items-center space-x-4">
+        <div>
+          <img
+            className="h-hidden md:inline h-10 w-10"
+            src={songInfo?.album.images?.[0].url}
+            alt={songInfo?.name}
+          />
+        </div>
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+
+      {/* center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon className="button" />
+
+        {isPlaying ? (
+          <PauseIcon className="button w-10 h-10" onClick={playPauseHandler} />
+        ) : (
+          <PlayIcon className="button w-10 h-10" onClick={playPauseHandler} />
+        )}
+
+        <FastForwardIcon className="button" />
+        <ReplyIcon className="button" />
+      </div>
+
+      {/* right - volumn */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon
+          className="button"
+          onClick={() => (volume) =>
+            0 && setVolume((prevState) => prevState - 10)}
+        />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          min={0}
+          max={100}
+          value={volume}
+          onChange={(e) => setVolume(+e.target.value)}
+        />
+        <VolumeUpIcon
+          className="button"
+          onClick={() =>
+            volume <= 100 && setVolume((prevState) => prevState + 10)
+          }
         />
       </div>
     </div>
